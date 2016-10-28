9260ce97a7fe8a7fb0b25b07940c96f89b9ccc5a
Hernan <nani93@gmail.com>
2016-10-27 21:21:24 -0300

    Proyecto Vacio Se creo el proyecto, con la configuracion de la placa. Solo tiene el archivo main vacio.

---
+++ main.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:20:32 10/27/2016 
+// Design Name: 
+// Module Name:    main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module main(
+    );
+
+
+endmodule
