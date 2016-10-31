be9ac3c3e622bf1c4a5414dc3f6f6a18eff5088a
Agustincou <cou_647@hotmail.com>
2016-10-31 12:59:33 -0300

    BLOQUE DE CONTROL - ESTRUCTURA GENERAL Primeras aproximaciones de los módulos del bloque de control en base a "Figure 3" of BIP 1 del paper "a basic processor for teaching digital circuits and systems design with fpga"

---
+++ Instruction_Decoder.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:52:51 10/31/2016 
+// Design Name: 
+// Module Name:    Instruction_Decoder 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Instruction_Decoder(
+    );
+
+
+endmodule
---
+++ main_control_block.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:51:10 10/31/2016 
+// Design Name: 
+// Module Name:    main_control_block 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module main_control_block(
+    );
+
+
+endmodule
